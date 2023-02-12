1f8a558a49b4d65d78fdf0bf734875b730a9f16e
João Capucho <jcapucho7@gmail.com>
2023-02-12 14:05:53 +0000

    explorer: Add oil configuration options

--- lua/modules/explorer/config.lua
+++ lua/modules/explorer/config.lua
@@ -1,4 +1,11 @@
-require("oil").setup()
+require("oil").setup({
+	skip_confirm_for_simple_edits = true,
+	keymaps = {
+		["q"] = "actions.close",
+		["<C-h>"] = "actions.parent",
+		["<C-l>"] = "actions.select",
+	},
+})
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
