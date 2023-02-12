8577497f91815878f20a0eb3316d4fa5ebfdee7b
João Capucho <jcapucho7@gmail.com>
2023-02-12 12:40:48 +0000

    lsp: configure python formatter

--- lua/modules/lsp/init.lua
+++ lua/modules/lsp/init.lua
@@ -11,13 +11,7 @@ use({
 	'jose-elias-alvarez/null-ls.nvim',
 	requires = { 'nvim-lua/plenary.nvim' },
 	config = function()
-		local null_ls = require("null-ls")
-
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.completion.spell,
-			},
-		})
+		require('modules.lsp.null-ls-config')
 	end
 })
 
