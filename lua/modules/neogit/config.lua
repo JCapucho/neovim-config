451309c1dfabd3e79f07a661acd45ca11c1c735f
João Capucho <jcapucho7@gmail.com>
2023-03-26 21:50:32 +0100

    Disable spell checking in neogit

--- lua/modules/neogit/config.lua
+++ lua/modules/neogit/config.lua
@@ -7,3 +7,8 @@ require("which-key").register({
 		["g"] = { "<Cmd>Neogit<CR>", "Open neogit" },
 	},
 }, { prefix = "<leader>" })
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "NeogitStatus", "NeogitPopup" },
+	callback = function() vim.opt_local.spell = false end,
+})
