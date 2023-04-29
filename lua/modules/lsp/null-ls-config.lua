9e69f2e749285f41a415af3bca7990bbc3dd4455
João Capucho <jcapucho7@gmail.com>
2023-04-29 23:03:09 +0100

    Use project prettier and eslint if available

--- lua/modules/lsp/null-ls-config.lua
+++ lua/modules/lsp/null-ls-config.lua
@@ -1,4 +1,5 @@
 local null_ls = require("null-ls")
+local null_ls_utils = require("null-ls.utils")
 local utils = require('modules.lsp.utils')
 
 local sources = {}
@@ -8,8 +9,22 @@ if IsModuleEnabled("languages.python") then
 end
 
 if IsModuleEnabled("web") then
-	table.insert(sources, null_ls.builtins.formatting.prettier)
-	table.insert(sources, null_ls.builtins.code_actions.eslint)
+	local fallbackPatterns = { "package.json", ".git" }
+	local root_pattern = function(...)
+		local fn = null_ls_utils.root_pattern(unpack(arg), unpack(fallbackPatterns))
+		return function(params)
+			return fn(params.bufname)
+		end
+	end
+
+	table.insert(sources, null_ls.builtins.formatting.prettier.with({
+		prefer_local = "node_modules/.bin",
+		cwd = root_pattern("prettier.config.cjs", "prettier.config.js"),
+	}))
+	table.insert(sources, null_ls.builtins.code_actions.eslint.with({
+		prefer_local = "node_modules/.bin",
+		cwd = root_pattern(".eslintrc.cjs", ".eslintrc.js")
+	}))
 end
 
 null_ls.setup({
