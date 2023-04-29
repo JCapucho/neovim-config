f824cbfbf7e63cbbcdf930376e96875fe0977c7d
João Capucho <jcapucho7@gmail.com>
2023-04-29 22:59:23 +0100

    Use null-ls when formatting source is available for buffer

--- lua/modules/lsp/utils.lua
+++ lua/modules/lsp/utils.lua
@@ -1,6 +1,17 @@
+local function is_null_ls_formatting_enabled(bufnr)
+	local file_type = vim.api.nvim_buf_get_option(bufnr, "filetype")
+	local generators = require("null-ls.generators").get_available(
+		file_type,
+		require("null-ls.methods").internal.FORMATTING
+	)
+	return #generators > 0
+end
+
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local function on_attach(client, bufnr)
+	local null_ls_formats = is_null_ls_formatting_enabled(bufnr)
+
 	if client.supports_method("textDocument/formatting") then
 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 		vim.api.nvim_create_autocmd("BufWritePre", {
@@ -10,6 +21,10 @@ local function on_attach(client, bufnr)
 				if vim.g.formatOnSave then
 					vim.lsp.buf.format({
 						bufnr = bufnr,
+						filter = function(formattingClient)
+							-- apply whatever logic you want (in this example, we'll only use null-ls)
+							return (not null_ls_formats) or formattingClient.name == "null-ls"
+						end,
 					})
 				end
 			end,
