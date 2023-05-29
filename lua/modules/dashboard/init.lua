e732f0702ba567bbba6c0dc9544e357d7d86336e
João Capucho <jcapucho7@gmail.com>
2023-05-29 21:04:11 +0100

    Update dashboard config

--- lua/modules/dashboard/init.lua
+++ lua/modules/dashboard/init.lua
@@ -1,8 +1,10 @@
 local use = require('packer').use
 
 use({
-	'glepnir/dashboard-nvim',
+	'nvimdev/dashboard-nvim',
+	event = 'VimEnter',
 	config = function()
 		require('modules.dashboard.config')
 	end,
+	requires = { 'nvim-tree/nvim-web-devicons' }
 })
