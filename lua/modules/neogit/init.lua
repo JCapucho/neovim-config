72a8e4a87ac74cc50eba719a953e409d480aca6f
João Capucho <jcapucho7@gmail.com>
2023-07-17 15:40:29 +0100

    Update neogit

--- lua/modules/neogit/init.lua
+++ lua/modules/neogit/init.lua
@@ -1,7 +1,7 @@
 local use = require('packer').use
 
 use({
-	'TimUntersberger/neogit',
+	'NeogitOrg/neogit',
 	requires = { 'nvim-lua/plenary.nvim' },
 	config = function()
 		require('modules.neogit.config')
