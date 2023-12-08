68446b61eac1e2e8b76f34699d8c13743350a157
João Capucho <jcapucho7@gmail.com>
2023-12-08 20:49:29 +0000

    Fix commentstring plugin integration

--- lua/modules/treesitter/config.lua
+++ lua/modules/treesitter/config.lua
@@ -48,10 +48,6 @@ require('nvim-treesitter.configs').setup({
 		enable = true,
 		additional_vim_regex_highlighting = false,
 	},
-	context_commentstring = {
-		enable = true,
-		enable_autocmd = false,
-	},
 	textobjects = {
 		select = {
 			enable = true,
@@ -137,6 +133,15 @@ require('rainbow-delimiters.setup')({
 		'RainbowDelimiterGreen',
 		'RainbowDelimiterViolet',
 		'RainbowDelimiterCyan',
+	},
+})
+
+vim.g.skip_ts_context_commentstring_module = true;
+
+require('ts_context_commentstring').setup({
+	enable_autocmd = false,
+	languages = {
+		d2 = '# %s',
 	},
 })
 
