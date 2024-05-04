43ba90dc348a9caf8966f709392910d8e90cb150
João Capucho <jcapucho7@gmail.com>
2024-05-04 18:11:54 +0100

    Add missing build step for luasnip

--- lua/modules/completion/plugins.lua
+++ lua/modules/completion/plugins.lua
@@ -6,6 +6,7 @@ local cmp_plugins = {
 	'hrsh7th/cmp-emoji',
 	{
 		'L3MON4D3/LuaSnip',
+		build = "make install_jsregexp",
 		dependencies = { 'rafamadriz/friendly-snippets', 'saadparwaiz1/cmp_luasnip' }
 	},
 	'onsails/lspkind.nvim',
